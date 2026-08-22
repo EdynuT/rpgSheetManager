8197439ae0be36b4aab2c220c487692f686e06c7
ALUNO VR SOFTWARE <avrsoftware@SITREDES.LOCAL>
2026-08-22 12:12:41 -0300

    Postagem de vinculação com db local

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-import java.sql.SQLException;
-
-import src.utils.DataBase;
+import src.gui.ConsoleGUI;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        DataBase.conectar();       
+    public static void main(String[] args) {
+        ConsoleGUI.exibirMenu();
     }
 }
--- src/utils/Util.java
+++ src/utils/Util.java
@@ -1,7 +1,15 @@
 package src.utils;
 
 public class Util {
-    public void diceParser() {
+    public void diceParser(String diceRoll) {
+        /*
+        Exemplo de entadas:
+        1d20
+        1d20 +(ou)- 5d10
+        1d20 *(ou)/ 5d10 + 10
+        2 / (1d20 + 5d10) * 10 
+        */
 
+        
     }
 }
